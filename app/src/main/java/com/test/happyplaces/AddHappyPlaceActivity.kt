fcab40ab59785afb183c836a6c369871d75599d7
Pratham Batra <pratham11.batra@gmail.com>
2023-11-11 22:06:56 +0530

    Added date picker for edit text date

--- app/src/main/java/com/test/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/test/happyplaces/AddHappyPlaceActivity.kt
@@ -1,11 +1,17 @@
 package com.test.happyplaces
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.test.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class AddHappyPlaceActivity : AppCompatActivity() {
     private var binding : ActivityAddHappyPlaceBinding? = null
+    private var calendar = Calendar.getInstance()
+    private lateinit var dateSetListener : DatePickerDialog.OnDateSetListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
@@ -16,5 +22,29 @@ class AddHappyPlaceActivity : AppCompatActivity() {
         binding?.toolbarAddPlace?.setNavigationOnClickListener {
             onBackPressedDispatcher.onBackPressed()
         }
+
+        dateSetListener = DatePickerDialog.OnDateSetListener {
+                view, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR,year)
+            calendar.set(Calendar.MONTH,month)
+            calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth)
+
+            updateDateInView()
+        }
+
+        binding?.etDate?.setOnClickListener {
+            DatePickerDialog(
+                this@AddHappyPlaceActivity,
+                dateSetListener,calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)).show()
+
+        }
+    }
+
+    private fun updateDateInView(){
+        val format =  "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(format, Locale.getDefault())
+        binding?.etDate?.setText(sdf.format(calendar.time).toString())
     }
 }
